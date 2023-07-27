021d686d8e876baf230643e83867feab00e7c1da
briongreene <brionalgreene@gmail.com>
2023-07-27 16:04:16 -0400

    wild wild life answered

--- homework.js
+++ homework.js
@@ -36,3 +36,37 @@ for(let f = 1; f <=100; f++) {
         console.log(f);
     }
 }
+
+// Wild Wild Life
+// Use the following arrays to answer the questions below (name,species ,age, hometown): You should be modifying the elements by accessing them. It is up to you which methods to use.
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+let name = ['Wolfy', 'Sharky', 'Plantee','Porgee',"D'Art"];
+let species = ['wolf', 'shark', 'plant', 'Porg', 'Demogorgan Dog'];
+let age = ['16', '20', '5000', '186', '2'];
+let hometown = ['Yukon Territory', 'Left Coast', 'Mordor', 'Ahch-To', 'Upside Down'];
+
+// console.log(name);
+// console.log(species);
+// console.log(age);
+// console.log(hometown);
+
+// Plantee just had her birthday; change Plantee's array to reflect her being a year older.
+plantee.splice(2, 1, '5001')
+console.log(plantee)
+// Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+
+wolfy.splice(3, 1, 'Gotham City')
+console.log(wolfy)
+// Give D'Art a second hometown by adding "Hawkins"
+
+dart.push('Hawkins')
+console.log(dart)
+// Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfyarray and replace it with "Gameboy".
+wolfy.splice(0, 1, 'Gameboy')
+console.log(wolfy)
+

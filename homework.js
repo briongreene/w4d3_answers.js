c7c53dd88a07ecb0cec24383a441ae0d399ea6dc
briongreene <brionalgreene@gmail.com>
2023-07-27 15:11:16 -0400

    get even answered

--- homework.js
+++ homework.js
@@ -5,3 +5,11 @@ for(let e = 1; e <=20; e++) {
     console.log(e)
 }
 
+// Get Even
+// Write a for loop that will log only the even numbers in 0 through 200.
+
+// Hint: Think about the increment expression.
+
+for(let g = 0; g <=200; g+= 2) {
+    console.log(g)
+}
